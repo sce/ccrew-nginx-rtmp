931d96f69bff4da96f79ec3ef6253a566558d170
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-11-12 19:03:12 +0100

    build.sh: Update help text.

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 docker build -t nginx_dual_rtmp .
 
-echo you can now do: docker run nginx_dual_rtmp
+echo -e "\nRun image with ./run.sh and then view log output with ./logs.sh"
+echo Kill container with ./kill.sh
